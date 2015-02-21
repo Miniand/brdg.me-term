717ba7908f3237d6168081a6cefc793f5fe25a09
Michael Alexander <beefsack@gmail.com>
2015-02-21 16:52:00 +1100

    Very cursory move to gocui.

--- main.go
+++ main.go
@@ -1,15 +1,18 @@
 package main
 
 import (
+	"fmt"
 	"log"
 	"os"
+
+	"github.com/jroimartin/gocui"
 
 	"code.google.com/p/goncurses"
 )
 
 var logger = log.New(os.Stderr, "", 0)
 
-func main() {
+func mainOld() {
 	if os.Getenv("ESCDELAY") == "" {
 		os.Setenv("ESCDELAY", "25")
 	}
@@ -24,4 +27,43 @@ func main() {
 	window.SetBackground(goncurses.ColorPair(ColorPairBackground))
 	window.Keypad(true)
 	NewWindowManager(window).Run()
+}
+
+func layout(g *gocui.Gui) error {
+	if v, err := g.SetView("center", 0, 0, 50, 50); err != nil {
+		if err != gocui.ErrorUnkView {
+			return err
+		}
+		v.Wrap = true
+		v.FgColor = gocui.ColorBlue | gocui.AttrBold
+		fmt.Fprintln(v, "This is a test, does this mean that text wraps? This is a real question.")
+		v.FgColor = gocui.ColorRed | gocui.AttrBold
+		fmt.Fprintln(v, "This is a test, does this mean that text wraps? This is a real question.")
+	}
+	return nil
+}
+
+func quit(g *gocui.Gui, v *gocui.View) error {
+	return gocui.Quit
+}
+
+func main() {
+	g := gocui.NewGui()
+	if err := g.Init(); err != nil {
+		log.Panic(err)
+	}
+	defer g.Close()
+
+	g.FgColor = gocui.ColorDefault
+	g.BgColor = gocui.ColorDefault
+
+	g.SetLayout(layout)
+
+	if err := g.SetKeybinding("", gocui.KeyCtrlC, gocui.ModNone, quit); err != nil {
+		log.Panic(err)
+	}
+
+	if err := g.MainLoop(); err != nil && err != gocui.Quit {
+		log.Panic(err)
+	}
 }
